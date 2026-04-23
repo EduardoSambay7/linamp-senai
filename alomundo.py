c2c636f25a500a5f89b933460c18b43b8dce5731
Eduardo Sambay Cavalari <duzeragrau@gmail.com>
2026-04-23 17:10:25 -0300

    tkinter

--- alomundo.py
+++ alomundo.py
@@ -4,5 +4,10 @@ janela = tk.Tk()
 janela.title("Player")
 janela.geometry("300x200")
 
+def play():
+    print("Play")
+
+btn = tk.Button(janela, text="Play", command=play)
+btn.pack()    
 janela.mainloop()
 
